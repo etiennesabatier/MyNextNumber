aa1115f3487e95c219e6a4e0d149476f602a16e1
Etienne SABATIER <etiennesabatier@gmail.com>
2015-06-18 10:48:40 -0400

    Initial Commit

---
+++ MyNextNumber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyNextNumber
+//
+//  Created by Etienne Sabatier on 18/06/2015.
+//  Copyright (c) 2015 TakeItEasy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
